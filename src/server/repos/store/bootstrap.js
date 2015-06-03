85b62eb2cb2111237e3cd48954ef000762106691
Antonio de Perio <antoniodeperio@gmail.com>
2015-06-04 08:54:29 +1000

    Adding auth provide table

--- src/server/repos/store/bootstrap.js
+++ src/server/repos/store/bootstrap.js
@@ -7,8 +7,14 @@ client.connect();
 var createUsers = 'CREATE TABLE IF NOT EXISTS ' +
   'users(' +
     'id SERIAL PRIMARY KEY, ' +
-    'auth_provider_id VARCHAR(255) UNIQUE not null, ' + //id supplied by auth proivder
+    'auth_provider_id_fkey VARCHAR(255) UNIQUE not null, ' + //id supplied by auth proivder
     'email_address VARCHAR(255) UNIQUE not null)';
+
+var createAuthProviders = 'CREATE TABLE IF NOT EXISTS ' +
+  'auth_providers(' +
+    'id SERIAL PRIMARY KEY, ' +
+    'provider_id VARCHAR(255) not null, ' + //a provider specific id
+    'type VARCHAR(255) UNIQUE not null)'; //ie google, twitter, facebook
 
 var createSessions = 'CREATE TABLE IF NOT EXISTS ' +
   'sessions(' +
@@ -20,6 +26,7 @@ var createSessions = 'CREATE TABLE IF NOT EXISTS ' +
     'created timestamp default current_timestamp)';
 
 client.query(createUsers);
+client.query(createAuthProviders);
 client.query(createSessions)
   .on('end', function() {
     client.end();
