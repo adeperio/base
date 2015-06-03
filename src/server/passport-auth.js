0c34fd4f41208821bcd02901164ff2e7a42dfa77
Antonio de Perio <antoniodeperio@gmail.com>
2015-06-03 18:38:46 +1000

    WIP on user create or edit

--- src/server/passport-auth.js
+++ src/server/passport-auth.js
@@ -2,7 +2,9 @@
 
 import passport from 'passport';
 import passportGoogleOauth2 from 'passport-google-oauth2';
+import UserRepository from './repos/store/UserRepository.js';
 var GoogleStrategy = passportGoogleOauth2.Strategy;
+
 
 passport.use(new GoogleStrategy({
   clientID:     Config.auth.clientID,
@@ -11,8 +13,14 @@ passport.use(new GoogleStrategy({
   passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
-    var user = {};
-    done(null, user);
+    var userRepo = new UserRepository();
+    console.log('PROFILE ' + JSON.stringify(profile));
+    return done(null, profile);
+    // userRepo.createUser({ googleId: profile.id }, function (err, user) {
+    //   return done(err, user);
+    // });
+
+
   }
 ));
 
