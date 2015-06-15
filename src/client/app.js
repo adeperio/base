f3ba32dba293829b55c26fbf975ebd4aae0cb702
Antonio de Perio <antoniodeperio@gmail.com>
2015-06-15 15:02:44 +1000

    Refactoring out routes map

--- src/client/app.js
+++ src/client/app.js
@@ -1,31 +1,8 @@
 'use strict';
 
-import 'babel/polyfill';
 import React from 'react';
-import emptyFunction from 'react/lib/emptyFunction';
-import App from './components/app';
-
-import Home from './components/home';
-import SignUp from './components/sign-up';
-import Login from './components/login';
-import Error from './components/error';
 import Router from 'react-router';
-
-var DefaultRoute = Router.DefaultRoute;
-var Link = Router.Link;
-var Route = Router.Route;
-var Redirect = Router.Redirect;
-var RouteHandler = Router.RouteHandler;
-
-var routes = (
-  <Route name="app" path="/" handler={App}>
-    <Route name="home" path="/home" handler={Home}/>
-    <Route name="signup" path="/signup" handler={SignUp}/>
-    <Route name="error" path="/error" handler={Error}/>
-    <Redirect from="/auth/google/callback" to="/home" />
-    <DefaultRoute handler={Login}/>
-  </Route>
-);
+import routes from './routes.js';
 
 function run() {
   Router.run(routes, Router.HistoryLocation, function (Handler) {
---
+++ src/client/routes.js
@@ -0,0 +1,28 @@
+'use strict'
+import 'babel/polyfill';
+import React from 'react';
+import App from './components/app';
+
+import Home from './components/home';
+import SignUp from './components/sign-up';
+import Login from './components/login';
+import Error from './components/error';
+import Router from 'react-router';
+
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var Redirect = Router.Redirect;
+var RouteHandler = Router.RouteHandler;
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="home" path="/home" handler={Home}/>
+    <Route name="signup" path="/signup" handler={SignUp}/>
+    <Route name="error" path="/error" handler={Error}/>
+    <Redirect from="/auth/google/callback" to="/home" />
+    <DefaultRoute handler={Login}/>
+  </Route>
+);
+
+module.exports = routes;
