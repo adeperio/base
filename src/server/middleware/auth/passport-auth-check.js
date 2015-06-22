1a2dffb83784a58fbe079d12e2c77dbfc1b759be
Antonio de Perio <antoniodeperio@gmail.com>
2015-06-22 12:01:48 +1000

    Adding req .user

--- src/server/middleware/auth/passport-auth-check.js
+++ src/server/middleware/auth/passport-auth-check.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function IsAuthenticated(req,res,next){
-    if(req.isAuthenticated()){
+    if(req.isAuthenticated() && req.user){
         next();
     }else{
         next(new Error(401));
