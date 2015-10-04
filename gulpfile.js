8ada5e067a3e6e7e549f31e5299c235d9cf03b7c
Antonio de Perio <antoniodeperio@gmail.com>
2015-10-04 22:57:20 +1100

    more instructional comments in dev gulp

--- gulpfile.js
+++ gulpfile.js
@@ -214,6 +214,7 @@ gulp.task('serve', ['build:watch'], function(cb) {
           });
           cb();
           console.log("Server started. Go to https://yourdomain.com:3000");
+          console.log("Remember to edit your hosts file to match your local host domain with the domain in your callback URL");
         }
       }
     });
