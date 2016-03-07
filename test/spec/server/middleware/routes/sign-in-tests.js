bcffb263591c52821103cefc3ac5b2b3060a2633
Antonio de Perio <antoniodeperio@gmail.com>
2016-03-07 20:41:46 +1100

    refactorign tests and testign for incorrect password

---
+++ test/spec/server/middleware/routes/sign-in-tests.js
@@ -0,0 +1,66 @@
+'use strict'
+
+import winston from 'winston';
+import chai from 'chai';
+import request from 'supertest';
+import appRoot from 'app-root-path';
+import express from 'express';
+import httpsAgent from 'superagent';
+import fs from 'fs';
+
+var assert = chai.assert;
+var expect = chai.expect;
+
+var RandomizerService = require(appRoot + '/src/server/services/randomizer-service.js');
+
+
+describe('User Sign In', function(){
+
+  describe('sign in user', function() {
+    it('should sign in existing user', function(done) {
+
+      var randomizer = new RandomizerService();
+      var mockPassword = randomizer.getRandomUUIDv4();
+      var mockEmail = randomizer.getRandomUUIDv4().substring(0,4) + '@test.com';
+
+      httpsAgent.post('https://basestackjs.com:3000/auth/signup')
+        .send({ emailAddress: mockEmail, password: mockPassword })
+        .end((err, res) => {
+
+            assert.equal(mockEmail, res.body.emailAddress);
+
+            httpsAgent.get('https://basestackjs.com:3000/auth/signin')
+              .send({ emailAddress: mockEmail, password: mockPassword })
+              .end((err, res) => {
+
+                  assert.isDefined(res.body, 'user was logged in');
+                  assert.equal(mockEmail, res.body.emailAddress);
+                  done();
+              });
+
+        });
+    })
+  });
+
+  describe('sign in user - incorrect password', function() {
+    it('should NOT allow a user to sign in with incorrect password', function(done) {
+
+      var randomizer = new RandomizerService();
+      var mockPassword = randomizer.getRandomUUIDv4();
+      var mockEmail = randomizer.getRandomUUIDv4().substring(0,4) + '@test.com';
+
+      httpsAgent.post('https://basestackjs.com:3000/auth/signup')
+        .send({ emailAddress: mockEmail, password: mockPassword })
+        .end((err, res) => {
+            assert.equal(mockEmail, res.body.emailAddress);
+            httpsAgent.get('https://basestackjs.com:3000/auth/signin')
+              .send({ emailAddress: mockEmail, password: 'boguspassword' })
+              .end((err, resSignIn) => {
+                  assert.isUndefined(resSignIn.body.emailAddress, 'user was not logged in');
+                  
+                  done();
+              });
+        });
+    })
+  });
+});
--- test/spec/server/middleware/routes/sign-up-tests.js
+++ test/spec/server/middleware/routes/sign-up-tests.js
@@ -13,8 +13,7 @@ var expect = chai.expect;
 
 var RandomizerService = require(appRoot + '/src/server/services/randomizer-service.js');
 
-
-describe('User Sign In', function(){
+describe('User Sign Up', function(){
 
   describe('sign up new user', function() {
     it('should return newly created user', function(done) {
@@ -35,29 +34,4 @@ describe('User Sign In', function(){
     })
   });
 
-  describe('sign in user', function() {
-    it('should sign in existing user', function(done) {
-
-      var randomizer = new RandomizerService();
-      var mockPassword = randomizer.getRandomUUIDv4();
-      var mockEmail = randomizer.getRandomUUIDv4().substring(0,4) + '@test.com';
-
-      httpsAgent.post('https://basestackjs.com:3000/auth/signup')
-        .send({ emailAddress: mockEmail, password: mockPassword })
-        .end((err, res) => {
-
-            assert.equal(mockEmail, res.body.emailAddress);
-
-            httpsAgent.get('https://basestackjs.com:3000/auth/signin')
-              .send({ emailAddress: mockEmail, password: mockPassword })
-              .end((err, res) => {
-
-                  assert.isDefined(res.body, 'user was logged in');
-                  assert.equal(mockEmail, res.body.emailAddress);
-                  done();
-              });
-
-        });
-    })
-  });
 });
