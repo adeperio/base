7973cb70f502905b52decaf62fefc5d7932eb85d
Antonio de Perio <antoniodeperio@gmail.com>
2016-03-07 23:15:37 +1100

    removign console logs

--- src/client/flux/actions/user-actions.js
+++ src/client/flux/actions/user-actions.js
@@ -12,19 +12,10 @@ module.exports = {
       .end((err, res) => {
 
         if(!err && !res.error) {
-          console.log("Did get me: SUCCESS");
-
-          Dispatcher.handleServerAction({
-            actionType: ActionTypes.ME_RES,
-            data: res.body
-          });
+          Dispatcher.handleServerAction({ actionType: ActionTypes.ME_RES, data: res.body });
 
         } else{
-          console.log("Did get me: ERROR");
-          Dispatcher.handleServerAction({
-            actionType: ActionTypes.ME_ERR,
-            data: res.error
-          });
+          Dispatcher.handleServerAction({ actionType: ActionTypes.ME_ERR, data: res.error });
         }
       });
   },
@@ -36,16 +27,14 @@ module.exports = {
       .accept('application/json')
       .end((err, res) => {
 
-        console.log("Did sign up: SUCCESS: " + JSON.stringify(res));
+
         if(!err && res && !res.error) {
-          console.log("Did sign up: SUCCESS");
           Dispatcher.handleServerAction({
             actionType: ActionTypes.SIGNUP_RES,
             data: res.body
           });
         } else {
 
-          console.log("Did sign up: ERROR ");
           Dispatcher.handleServerAction({
             actionType: ActionTypes.SIGNUP_ERR,
             data: res.error
@@ -61,11 +50,9 @@ module.exports = {
       .end((err, res) => {
 
         if(!err && !res.error) {
-          console.log("Did signout: SUCCESS");
           Dispatcher.handleServerAction({ actionType: ActionTypes.SIGNOUT_RES, data: res.body
           });
         } else {
-          console.log("Did signout: ERROR");
           Dispatcher.handleServerAction({ actionType: ActionTypes.SIGNOUT_ERR, data: res.error });
         }
       });
